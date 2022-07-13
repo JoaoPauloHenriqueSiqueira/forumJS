a4287b902f4b43be6ce21d634df7ca1201123daa
JoaoPauloHenriqueSiqueira <joao.henriquesiqueirati@gmail.com>
2022-07-13 13:31:01 -0300

    Question Model created, screen for crud wip

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express")
+
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/config');
+const questionModel = require('./database/Models/Question');
+
+//Database
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("AQUI DEU BOM")
+    })
+    .catch((msgError) =>
+    {
+        console.log("AN error occurred")
+    })
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+let nome = "João";
+let msg = true;
+let produtos = [
+    { nome:"Doritos", preco:3.15},
+    { nome: "Fandangos", preco: 2.15 },
+    { nome: "Churros", preco: 11.08 },
+]
+
+app.get("/", (req,res)=>{
+    res.render("index", {
+        nome:nome,
+        msg: msg,
+        produtos: produtos
+    })
+})
+
+app.get("/questions", (req, res) => {
+    res.render("questions", {
+        nome: nome,
+        msg: msg,
+        produtos: produtos
+    })
+})
+
+app.post("/saveQuestion", (req, res) => {
+    res.send(req.body.question);
+})
+
+app.listen(8080, ()=>{
+    console.log("APP RODANDO");
+})
